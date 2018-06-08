ea0fde4700b459100d032d43e4cac5b6e081de8e
Madhav Paudel <poudelmadhav143@gmail.com>
2018-06-08 21:00:04 +0545

    link to user page added

--- app/models/donorform.rb
+++ app/models/donorform.rb
@@ -1,5 +1,5 @@
 class Donorform < ApplicationRecord
   belongs_to :user
   validates :title, :description, :promises, :amount, :deadline, presence: true
-  delegate :name, to: :user, prefix: true
+  delegate :name, :id, to: :user, prefix: true
 end
