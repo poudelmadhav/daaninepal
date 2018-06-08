332499044a730355dde9bc6ea63ea1538b6583f3
Madhav Paudel <poudelmadhav143@gmail.com>
2018-06-08 22:24:01 +0545

    user to donor form

--- app/controllers/donorforms_controller.rb
+++ app/controllers/donorforms_controller.rb
@@ -26,7 +26,7 @@ class DonorformsController < ApplicationController
   # POST /donorforms.json
   def create
     @donorform = Donorform.new(donorform_params)
-    @donorform = current_user
+    @donorform.user = current_user
 
     respond_to do |format|
       if @donorform.save
