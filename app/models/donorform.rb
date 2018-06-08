56154ace052d44bbb38ce8472c2e5044e3022b4e
Madhav Paudel <poudelmadhav143@gmail.com>
2018-06-08 22:50:53 +0545

    count the total request donation of user shown in profile page

--- app/models/donorform.rb
+++ app/models/donorform.rb
@@ -1,5 +1,5 @@
 class Donorform < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   validates :title, :description, :promises, :amount, :deadline, presence: true
   delegate :name, :id, to: :user, prefix: true
 end
