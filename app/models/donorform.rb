a96c80b356ce95c02355aa638943f76c44e8db8c
Madhav Paudel <poudelmadhav143@gmail.com>
2018-06-09 09:40:37 +0545

    bootstrap tab added

--- app/models/donorform.rb
+++ app/models/donorform.rb
@@ -3,4 +3,8 @@ class Donorform < ApplicationRecord
   validates :title, :description, :promises, :amount, :deadline, presence: true
   delegate :name, :id, to: :user, prefix: true
   has_many :donations
+
+ #  def is_valid_donation(donation)
+	# return  @donation.amount - @donation.donations.sum("amount")
+ #  end
 end
