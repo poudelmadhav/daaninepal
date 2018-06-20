db7864dc78401a0de022457bae45be8557604888
Madhav Paudel <poudelmadhav143@gmail.com>
2018-06-20 15:04:07 +0545

    trying to fix invalid authenticity token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  protect_from_forgery with: :null_session //fix invalid autheniticity token error
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+	end
 end
