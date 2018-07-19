56d7b8c59f3feb1430acda6ce3a4146c6f0eb02a
Madhav Paudel <poudelmadhav143@gmail.com>
2018-07-19 12:12:43 +0545

    refactoring code to send notification

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,11 +2,11 @@ class DonationsController < ApplicationController
 	def create
 		@donorform = Donorform.find(params["donorform_id"])
 		@donation = @donorform.donations.create(donation_params.merge(user_id: current_user.id))
-		if @donation.valid?
+		if @donation.valid? && donor = UserNotificationService.new(current_user)
 			flash[:success] = "Donation Successful!"
 			# send notification
-			UserNotificationService.new(current_user).notify_donor
-			UserNotificationService.new(current_user).notify_receiver(@donorform.user, @donation)
+			donor.notify_donor
+			donor.notify_receiver(@donorform.user, @donation)
 			redirect_to donorform_path(@donorform)
 		else
 			flash[:alert] = "Invalid Amount!"
