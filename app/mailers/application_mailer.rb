239f367f2d2f3c62f4fc18d0e18192596874edb0
Madhav Paudel <poudelmadhav143@gmail.com>
2025-11-24 20:03:39 +0545

    Mail settings updated

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV.fetch("DEFAULT_FROM_EMAIL") { "Daani Nepal<noreply@paudelmadhav.com.np>" }
   layout 'mailer'
 end
