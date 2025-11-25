b488a206ff6275a4848c1eafb3b2ccade53b77e0
Madhav Paudel <poudelmadhav143@gmail.com>
2025-11-25 14:47:39 +0545

    Preserve display name in email

--- config/initializers/resend_delivery_method.rb
+++ config/initializers/resend_delivery_method.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'json'
+require 'base64'
 
 class ResendDeliveryMethod
   attr_accessor :settings
@@ -13,8 +14,20 @@ class ResendDeliveryMethod
     raise 'RESEND_API_KEY environment variable not set' unless api_key
 
     # Prepare email data
+    # Preserve full "Display Name <email@domain.com>" format for from field
+    from_field = if mail.from&.any?
+                   # Check if mail.from contains display name format
+                   if mail[:from].display_names.any? && mail[:from].addresses.any?
+                     "#{mail[:from].display_names.first} <#{mail[:from].addresses.first}>"
+                   else
+                     mail.from.first
+                   end
+                 else
+                   'noreply@example.com'
+                 end
+
     email_data = {
-      from: mail.from&.first || 'noreply@example.com',
+      from: from_field,
       to: mail.to,
       subject: mail.subject,
       html: mail.html_part&.body&.decoded || mail.body.decoded,
@@ -27,6 +40,17 @@ class ResendDeliveryMethod
 
     # Add reply-to if present
     email_data[:reply_to] = mail.reply_to if mail.reply_to&.any?
+
+    # Add attachments if present
+    if mail.attachments.any?
+      email_data[:attachments] = mail.attachments.map do |attachment|
+        {
+          filename: attachment.filename,
+          content: Base64.strict_encode64(attachment.body.decoded),
+          content_type: attachment.content_type
+        }
+      end
+    end
 
     # Remove nil values
     email_data.compact!
